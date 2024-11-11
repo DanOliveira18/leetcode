2f274053293151fca8de6c90d9da0a4dae30927b
Daniel Oliveira <daniel.santos@venturus.org.br>
2024-11-10 21:28:36 -0300

    big O notation

---
+++ src/base/bigONotation/bigONotation.ts
@@ -0,0 +1,82 @@
+// big O é uma notação para saber o quão eficiente é o seu algoritmo em questão de espaço e tempo
+// Sempre considerar o maior big O da função para definir em qual ela se encaixa
+
+// Ranking de complexidade do O(n)
+// O(1), O(log n) > O(n) > O(n log n) > O(n^2) > O(2^n) > O(n!)
+
+// O(1);
+// Independente do tamanho da lista, sempre vai printar o primeiro, portanto a performance é constante
+const printFirst = (items: Array<any>) => {
+  console.log(items[0]); // O(1);
+};
+
+// O(n)
+// A performance varia de acordo com o tamanho da lista, portanto é um crescimento linear
+const printAll = (items: Array<any>) => {
+  //O(n)
+  items.forEach((item) => {
+    console.log(item); //O(1)
+  });
+};
+
+// O(n)
+// Apesar de ter 2 iterações separadas, ambas são O(n) pois percorrem o array 1 vez cada
+// Como a notação big O pega o maior big O da função, e o maior é O(n), então essa função
+// é O(n)
+const printAllAndInverted = (items: Array<any>) => {
+  //O(n)
+  items.forEach((item) => {
+    console.log(item); //O(1)
+  });
+
+  console.log("--------");
+
+  //O(n)
+  items.reverse().forEach((item) => {
+    console.log(item); //O(1)
+  });
+};
+
+// O(n) n = numero de células
+// Apesar de possui um for dentro de outro for, pode-se considerar essa função O(n)
+// por conta de que o 2 for vai depender do numero de colunas e não necessariamente vai ser
+// exatamente o mesmo valor de linhas. Portanto podemos dizer que essa função é O(n) onde n é
+// o numero de células dessa matrix
+const findElement = (matrix: Array<Array<number>>, target: number) => {
+  const rows = matrix.length; // O(1)
+  const cols = matrix[0].length; // O(1)
+
+  // O(n) n = numero de células na matrix
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] == target) {
+        console.log(`target found at row: ${i + 1} and col: ${j + 1}`);
+        return;
+      }
+    }
+  }
+
+  console.log("target not found");
+};
+
+// --- TEST CASES
+const items = [5, 6, 7, 8, 9, 3, 4, 6, 7];
+
+// console.log("Print first");
+// printFirst(items);
+// console.log("Print All");
+// printAll(items);
+// console.log("Print All and Inverted");
+// printAllAndInverted(items);
+
+// const matrix = [
+//   [0, 1],
+//   [2, 3],
+//   [4, 5],
+//   [6, 7],
+//   [8, 9],
+//   [10, 11],
+// ];
+
+// console.log("Find Element");
+// findElement(matrix, 11);
---
+++ src/base/bubbleSort/bubbleSort.ts
@@ -0,0 +1,32 @@
+// Compara o elemento atual com o proximo, caso seja maior troca de posição, repete isso até o arr não realizar mais nenhuma mudança
+// Melhor caso: O(n) quando a lista ja está ordenada, ainda necessita percorrer a lista 1 vez
+// Caso medio e pior caso: O(n)² onde será necessario percorrer todo o array
+
+const bubbleSort = (arr: Array<number>) => {
+  console.log("start array: ", arr.toString());
+
+  // Inicia com "tamanho do array - 1" e vai decrescendo pq na primeira iteração o maior elemento ja vai para a ultima posição
+  // Portanto não necessita ser ordenado
+  for (let i = arr.length - 1; i > 0; i--) {
+    //Percorre o array jogando o maior elemento para a ultima posição
+    let swap = false;
+    for (let j = 0; j < arr.length; j++) {
+      //Se o elemento atual for maior que o proximo: swap
+      if (arr[j] > arr[j + 1]) {
+        console.log(`[i: ${i}, j: ${j}] Swap: ${arr[j]} > ${arr[j + 1]}`);
+        const aux = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = aux;
+        swap = true;
+      }
+    }
+    // if (!swap) break; // Otimização, se não houver nenhuma mudança, para de percorrer o array pois ja está ordenado
+    console.log(`[i: ${i}] array: ${arr.toString()}`);
+  }
+
+  console.log("final array: ", arr.toString());
+};
+
+const arr = [5, 4, 3, 2];
+//const arr = [6, 7, 8, 1, 2, 3, 4, 5, 6];
+bubbleSort(arr);
